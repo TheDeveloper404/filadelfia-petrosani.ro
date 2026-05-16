de80e7fa9140747ae899c22a68b8d030d7cb5b0f
Liviu <dev.workspacehub@gmail.com>
2026-05-16 21:05:49 +0300

    change LCP

--- vite.config.ts
+++ vite.config.ts
@@ -38,7 +38,7 @@ export default defineConfig({
         ],
       },
       workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+        globPatterns: ['**/*.{js,css,html,ico,png,jpg,jpeg,webp,svg,woff2}'],
         navigateFallback: 'index.html',
       },
     }),
